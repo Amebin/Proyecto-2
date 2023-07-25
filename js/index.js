dba893f1abf111e3e5c8c8f301c3df37ba19d1d3
Segovia Anibal Miguel <segoviaanibal@gmail.com>
2023-07-24 23:06:15 -0300

    Commit 4

--- js/index.js
+++ js/index.js
@@ -1,18 +1,22 @@
 const formulario = document.getElementById('formulario');
 const inputs = document.querySelectorAll('#formulario input');
 
+//================================================================================
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
     apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
     especialidad: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
     matricula: /^\d{3,14}$/, // 7 a 14 numeros.
-    domicilio: /^[a-zA-Z0-9\s\:]{4,40}$/, // Letras y numeros 
+    domicilio: /^[a-zA-Z0-9\s\:]{4,40}$/, // Letras y numeros .
 	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 	telefono: /^\d{7,14}$/, // 7 a 14 numeros.
-	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
+	usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo.
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 }
 
+//================================================================================
+
+//Poscion del campo
 const campos = {
 	nombre: false,
 	apellido: false,
@@ -25,46 +29,64 @@ const campos = {
 	password: false,
 }
 
-const validarFormulario = (e) => {
-	switch (e.target.name) {
+//================================================================================
+
+//contructor de objeto
+class Medico {
+	constructor(nombre, apellido, especialidad, matricula, domicilio, telefono, correo, usuario, password, password2){
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.especialidad = especialidad;
+		this.matricula = matricula;
+		this.domicilio = domicilio;
+		this.telefono = telefono;
+		this.correo = correo;
+		this.usuario = usuario;
+		this.password = password;
+		this.password2 = password2;
+	}
+}
+//================================================================================
+
+const validarFormulario = (evento) => {
+	switch (evento.target.name) {
         case "nombre":
-            validarCampo(expresiones.nombre, e.target, 'nombre');
+            validarCampo(expresiones.nombre, evento.target, 'nombre');			
         break;
         case "apellido":
-            validarCampo(expresiones.apellido, e.target, 'apellido');
+            validarCampo(expresiones.apellido, evento.target, 'apellido');			
         break;
         case "especialidad":
-            validarCampo(expresiones.especialidad, e.target, 'especialidad');
+            validarCampo(expresiones.especialidad, evento.target, 'especialidad');
         break;
         case "matricula":
-            validarCampo(expresiones.matricula, e.target, 'matricula');
+            validarCampo(expresiones.matricula, evento.target, 'matricula');			
         break;
         case "domicilio":
-            validarCampo(expresiones.domicilio, e.target, 'domicilio');
+            validarCampo(expresiones.domicilio, evento.target, 'domicilio');			
         break;
         case "telefono":
-            validarCampo(expresiones.telefono, e.target, 'telefono');
+            validarCampo(expresiones.telefono, evento.target, 'telefono');			
         break;
-		case "usuario":
-			validarCampo(expresiones.usuario, e.target, 'usuario');
+		case "correo":
+			validarCampo(expresiones.correo, evento.target, 'correo');			
 		break;
-        case "correo":
-            validarCampo(expresiones.correo, e.target, 'correo');
-        break;
+		case "usuario":
+			validarCampo(expresiones.usuario, evento.target, 'usuario');			
+		break;
         case "password":
-            validarCampo(expresiones.password, e.target, 'password');
+            validarCampo(expresiones.password, evento.target, 'password');			
             validarPassword2();
+			
         break;
         case "password2":
-            validarPassword2();
+			validarPassword2();			
         break;
-    }
-    }
+    }	
+}
 
-
-
-
-
+//================================================================================
+// funcion para validar los campos
 const validarCampo = (expresion, input, campo) => {
 	if(expresion.test(input.value)){
 		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
@@ -82,6 +104,8 @@ const validarCampo = (expresion, input, campo) => {
 		campos[campo] = false;
 	}
 }
+
+//================================================================================
 
 const validarPassword2 = () => {
 	const inputPassword1 = document.getElementById('password');
@@ -104,18 +128,36 @@ const validarPassword2 = () => {
 	}
 }
 
+//================================================================================
+
 inputs.forEach((input) => {
 	input.addEventListener('keyup', validarFormulario);
 	input.addEventListener('blur', validarFormulario);
 });
 
-formulario.addEventListener('submit', (e) => {
-	e.preventDefault();
+//================================================================================
 
+formulario.addEventListener('submit', (evento) => {
+	evento.preventDefault();
 	const terminos = document.getElementById('terminos');
-	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
-		formulario.reset();
-
+	if(campos.nombre && campos.apellido && campos.especialidad && campos.matricula && campos.domicilio && campos.telefono && campos.correo && campos.usuario && campos.password && terminos.checked){
+		const nombre = document.getElementById('nombre').value;
+		const apellido = document.getElementById('apellido').value;
+		const especialidad = document.getElementById('especialidad').value;
+		const matricula = document.getElementById('matricula').value;
+		const domicilio = document.getElementById('domicilio').value;
+		const telefono = document.getElementById('telefono').value;
+		const correo = document.getElementById('correo').value;
+		const usuario = document.getElementById('usuario').value;
+		const password = document.getElementById('password').value;
+		const password2 = document.getElementById('password2').value;
+		//Crea un nuevo objeto medico
+		const Nuevomedico = new Medico (nombre, apellido, especialidad, matricula, domicilio, telefono,correo, usuario, password, password2);
+		
+		localStorage.setItem('ListaMedicos', JSON.stringify(Nuevomedico)); // almacena el Obejto 
+		
+		formulario.reset(); // Pone en blanco el formulario
+		
 		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
 		setTimeout(() => {
 			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
