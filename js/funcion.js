0c6399c2c2b409f69d7694f30b7d9bc8d5918823
olea27 <olea994@gmail.com>
2023-07-31 18:29:09 -0300

    ultimos CAMBIOS

--- js/funcion.js
+++ js/funcion.js
@@ -1,3 +1,6 @@
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css");
+
+
 document.addEventListener('DOMContentLoaded',()=>{
     const carrusel = document.querySelector('.carrusel');
     const iner = document.querySelector('.carrusel-iner');
